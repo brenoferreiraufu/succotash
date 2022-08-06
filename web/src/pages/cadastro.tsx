82429f66e77a18de73001ac5185a9e69db46414d
vinilopes91 <vinilopes13@hotmail.com>
2022-08-06 01:46:43 -0300

    feat: paginas de login e cadastro

---
+++ web/src/pages/cadastro.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import {
+  Box,
+  Flex,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  IconButton
+} from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+type RegisterForm = {
+  fullName: string
+  username: string
+  password: string
+  confirmPassword: string
+}
+
+const Home: NextPage = () => {
+  const [registerFormValues, setRegisterFormValues] = useState<RegisterForm>({
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const router = useRouter()
+
+  const passwordMatch = registerFormValues.password === registerFormValues.confirmPassword
+
+  const handleClickPasswordButton = () => setShowPassword(!showPassword)
+  const handleClickConfirmPasswordButton = () => setShowConfirmPassword(!showConfirmPassword)
+
+  const handleInputChange = (field: keyof RegisterForm) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterFormValues((oldFormValues) => ({
+      ...oldFormValues,
+      [field]: e.target.value
+    }))
+  }
+
+  const handleSubmitForm = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (passwordMatch) {
+      console.table(registerFormValues)
+    } else {
+      console.log('TODO alerta')
+    }
+  }
+
+  return (
+    <Flex height="100vh" justifyContent="center" position="relative">
+      <IconButton
+        aria-label="Voltar página"
+        icon={<ChevronLeftIcon />}
+        onClick={() => router.back()}
+        position="absolute"
+        left={8}
+        top={5}
+      />
+      <Flex direction="column" px={8} py={5} rounded={6} alignItems="center" justifyContent="center" width="full">
+        <img src="/images/logo_transparent.png" alt="Foto de um restaurante" width={250} />
+        <Box as="form" onSubmit={handleSubmitForm} mt={-5}>
+          <FormControl isRequired>
+            <FormLabel>Nome completo</FormLabel>
+            <Input
+              value={registerFormValues.fullName}
+              placeholder="Digite seu nome completo"
+              onChange={handleInputChange('fullName')}
+            />
+          </FormControl>
+          <FormControl mt={2} isRequired>
+            <FormLabel>Usuário</FormLabel>
+            <Input
+              value={registerFormValues.username}
+              placeholder="Digite seu usuário"
+              onChange={handleInputChange('username')}
+            />
+          </FormControl>
+          <FormControl mt={2} isRequired>
+            <FormLabel>Senha</FormLabel>
+            <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Digite sua senha"
+                value={registerFormValues.password}
+                onChange={handleInputChange('password')}
+              />
+              <InputRightElement width="6.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClickPasswordButton}>
+                  {showPassword ? 'Esconder' : 'Mostrar'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <FormControl mt={2} isRequired isInvalid={!passwordMatch}>
+            <FormLabel>Confirmar senha</FormLabel>
+            <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                type={showConfirmPassword ? 'text' : 'password'}
+                placeholder="Confirme sua senha"
+                value={registerFormValues.confirmPassword}
+                onChange={handleInputChange('confirmPassword')}
+              />
+              <InputRightElement width="6.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClickConfirmPasswordButton}>
+                  {showConfirmPassword ? 'Esconder' : 'Mostrar'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <FormErrorMessage>Senhas diferentes</FormErrorMessage>
+          </FormControl>
+          <Button type="submit" colorScheme="blue" mt={5} width="full">
+            Cadastrar
+          </Button>
+        </Box>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Home
