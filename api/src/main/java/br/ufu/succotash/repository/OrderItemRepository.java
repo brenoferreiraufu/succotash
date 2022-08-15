8e53a1ad1a1633c9f7247e35fa8d40e5b906a8a2
brenoferreiraufu <breno.ferreira@ufu.br>
2022-08-14 23:54:05 -0300

    feat: payment

--- api/src/main/java/br/ufu/succotash/repository/OrderItemRepository.java
+++ api/src/main/java/br/ufu/succotash/repository/OrderItemRepository.java
@@ -1,8 +1,15 @@
 package br.ufu.succotash.repository;
 
+import br.ufu.succotash.domain.model.Order;
 import br.ufu.succotash.domain.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 @Repository
-public interface OrderItemRepository  extends JpaRepository<OrderItem, String> {}
+public interface OrderItemRepository  extends JpaRepository<OrderItem, String> {
+    List<OrderItem> findByOrder(Order order);
+}
--- api/src/main/java/br/ufu/succotash/service/OrderService.java
+++ api/src/main/java/br/ufu/succotash/service/OrderService.java
@@ -1,14 +1,19 @@
 package br.ufu.succotash.service;
 
 import br.ufu.succotash.controller.order.request.OrderRequest;
+import br.ufu.succotash.domain.enumeration.OrderStatus;
+import br.ufu.succotash.domain.model.Order;
 import br.ufu.succotash.domain.model.OrderItem;
-import br.ufu.succotash.domain.model.User;
+import br.ufu.succotash.mock.MockPaymentClient;
+import br.ufu.succotash.mock.MockPaymentRequest;
 import br.ufu.succotash.repository.OrderItemRepository;
 import br.ufu.succotash.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -16,23 +21,43 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
-
-
+    private final MockPaymentClient paymentClient;
 
     public String newOrder(OrderRequest order) {
 
-        orderRepository.save(order.toModel().get(0).getOrder());
+        var id = orderRepository.save(order.toModel().get(0).getOrder()).getId();
 
-        for(OrderItem orderItem : order.toModel()) {
+        for (OrderItem orderItem : order.toModel()) {
             orderItemRepository.save(orderItem);
-       }
+        }
 
-        return "";
+        return id;
     }
 
-    public Optional<User> findOrder(String orderId) {
-        return null;
+    public Order findOrder(String orderId) {
+        return orderRepository.findById(orderId).orElseThrow(() -> {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        });
     }
 
+    public void payOrder(String orderId) {
+        var order = findOrder(orderId);
+        var orderItems = orderItemRepository.findByOrder(order);
 
+        var total = new BigDecimal(0);
+        for (OrderItem orderItem : orderItems) {
+            var price = orderItem.getItem().getPrice();
+            var quantity = new BigDecimal(orderItem.getQuantity());
+            total = total.add(price.multiply(quantity));
+        }
+
+        var response = paymentClient.pay(new MockPaymentRequest(total, order.getId()));
+
+        if (response == HttpStatus.BAD_REQUEST) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+        }
+
+        order.setStatus(OrderStatus.COMPLETED);
+        orderRepository.save(order);
+    }
 }
