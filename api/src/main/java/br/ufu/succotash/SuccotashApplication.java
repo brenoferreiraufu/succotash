9e724834eafbd8e3698fd243fb74c1f561559961
Euller Henrique <48317736+EullerHenrique@users.noreply.github.com>
2022-08-16 21:49:41 -0300

    Update SuccotashApplication.java

--- api/src/main/java/br/ufu/succotash/SuccotashApplication.java
+++ api/src/main/java/br/ufu/succotash/SuccotashApplication.java
@@ -13,6 +13,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.math.BigDecimal;
 @EnableWebMvc
@@ -59,6 +61,19 @@ public class SuccotashApplication {
 		}
 
 
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+			}
+		};
 	}
 
 }
